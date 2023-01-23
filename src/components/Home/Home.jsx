ceca3e64b0a97aae03948d519d308394e3916788
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-01-23 12:44:36 -0600

    tailwind install

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
 // Components
+import NavHeader from '../NavHeader/NavHeader'
 import Search from '../Search/Search'
 
 const Home = () => {
   return (
     <div>
-      <h1>Pokemon React!</h1>
+      <h1 class='text-3xl font-bold text-center'>Pokemon React!</h1>
 
       <div>
         <Search />
---
+++ src/components/NavHeader/NavHeader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavHeader = () => {
+  return (
+    <div>NavHeader</div>
+  )
+}
+
+export default NavHeader
