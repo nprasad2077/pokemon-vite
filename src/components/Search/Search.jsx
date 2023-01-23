b0f4f4b6a7b59db833da3f0641261d18712b677c
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-01-23 13:13:37 -0600

    pokesearch works!

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -3,12 +3,30 @@ import axios from 'axios'
 import { useState, useEffect } from 'react'
 
 const Search = ({pokemon, setPokemon}) => {
+  const [search, setSearch] = useState('')
+
+  const handleInput = (e) => {
+    e.preventDefault()
+    setSearch(e.target.value)
+  }
+
+  const getPokemon = async () => {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${search}`)
+    .then(res => setPokemon(res.data))
+    .catch(err => console.log(err))
+  }
 
   console.log(pokemon);
+  console.log(search);
+
 
 
   return (
-    <div>Search</div>
+    <div class='flex flex-col mt-5'>
+      <p class='text-center'>Search Pokémon:</p>
+      <input type='text' placeholder='Search Pokemon' class='mx-20' onChange={handleInput} />
+      <button onClick={getPokemon}>Submit</button>
+    </div>
   )
 }
 
