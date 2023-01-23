0eecfd4056244dacfbbe334051ad49a2d8ca5e71
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-01-23 12:54:22 -0600

    pokeapi get call success

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 const Search = () => {
+  const pokeUrl = 'https://pokeapi.co/api/v2/pokemon/ditto'
+  const [pokemon, setPokemon] = useState([])
+
+  const getPokemon = async () => {
+    const response = await axios.get(pokeUrl)
+    console.log(response.data)
+  }
+
+  useEffect(() => {getPokemon()}, [])
+
+
   return (
     <div>Search</div>
   )
