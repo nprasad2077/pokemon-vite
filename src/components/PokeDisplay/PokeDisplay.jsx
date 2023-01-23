c757f13964333480cc8333607dde0d8715c88d58
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-01-23 14:12:08 -0600

    red version sprites

--- src/components/PokeDisplay/PokeDisplay.jsx
+++ src/components/PokeDisplay/PokeDisplay.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 const PokeDisplay = ({sprites}) => {
 
+    const redVersion = sprites && sprites.versions['generation-i']['red-blue']
+
+    console.log(redVersion);
+
 
   return (
     <div class='flex flex-col'>
@@ -13,9 +17,14 @@ const PokeDisplay = ({sprites}) => {
             <img src={sprites ? sprites.back_default : null} alt='back' />
         </div>
         <p>Shiny Sprites</p>
-        <div class='flex flwx-row'>
+        <div class='flex flex-row'>
             <img src={sprites ? sprites.front_shiny : null} alt='front shiny' />
             <img src={sprites ? sprites.back_shiny : null} alt='back shiny' />
+        </div>
+        <p>Blue/Red Version Sprites</p>
+        <div class='flex flex-row'>
+            <img src={redVersion ? redVersion.front_default : null} alt='front red/blue' />
+            <img src={redVersion ? redVersion.back_default : null} alt='back red/blue' />
         </div>
         
     
