3eabb7473cd7a623334761242294b0070db42729
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-01-23 12:38:29 -0600

    create search and home page.

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+// Components
+import Search from '../Search/Search'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Pokemon React!</h1>
+
+      <div>
+        <Search />
+      </div>
+    </div>
+  )
+}
+
+export default Home
