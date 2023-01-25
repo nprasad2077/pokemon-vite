20c19353ada2d6164c8edfddbf4eba5a14e0bf33
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-01-25 16:59:11 -0600

    emerald theme 🧪

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,6 +8,10 @@ import PokeDisplay from '../PokeDisplay/PokeDisplay'
 const Home = ({pokemon, setPokemon, getPokemon}) => {
 
   const sprites = pokemon.sprites
+  // const name = pokemon && pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
+
+
+  console.log(name);
 
 
 
@@ -18,6 +22,7 @@ const Home = ({pokemon, setPokemon, getPokemon}) => {
       <div>
         <div class='flex flex-col content-center items-center'>
           <Search pokemon={pokemon} setPokemon={setPokemon}/>
+          <p>{name}</p>
         </div>
         <div class='flex flex-row'>
           <PokeDisplay sprites={sprites}/>
